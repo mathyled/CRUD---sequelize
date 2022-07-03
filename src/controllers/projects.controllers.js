6464b529b2ff8f3f609552921cfd14719e71ae55
unknown <mathiasimagine@gmail.com>
2022-07-02 21:56:07 -0300

    feat change async await to promises.then()

--- src/controllers/projects.controllers.js
+++ src/controllers/projects.controllers.js
@@ -2,108 +2,115 @@
 import { Project } from "../models/Project.js"
 import { Task } from "../models/Task.js";
 
-export const getProjects = async (req, res) => {
+export const getProjects = (req, res) => {
 
     try {
         // throw new Error("Api failed") para probar errores
-        const projects = await Project.findAll()
+        Project.findAll().then(response => {
+            res.status(200).json(response)
+        })
         // console.log(projects.length);
         // res.status(200).send("Getting projects")
-        res.status(200).json(projects)
     } catch (error) {
         res.status(500).json({ message: error.message })
     }
-
 };
-export const createProject = async (req, res) => {
+
+export const createProject = (req, res) => {
     try {
         const { name, priority, description } = req.body
 
-        const newProject = await Project.create({ name, priority, description })
-        console.log("Creating projects");
-        res.status(200).json(newProject)
+        Project.create({ name, priority, description }).then(response => {
+            console.log("Creating projects");
+            res.status(200).json(response)
+        })
     } catch (error) {
         res.status(500).json({ message: error.message })
-
     }
-
 };
 
-export const updateProject = async (req, res) => {
+export const updateProject =  (req, res) => {
     const { id } = req.params
     const { name, priority, description } = req.body
 
 
     // 1ra forma
     const update = { name, priority, description }
-    const projectUpdate = await Project.update(update, {
+    Project.update(update, {
         where: {
             id: id
         }
-    });
-    // console.log(projectUpdate);
-    // res.json(projectUpdate);
+    }).then(json => {
+        // console.log(json);
+        // res.json(json);
+    })
 
     // 2da forma
-    const project = await Project.findByPk(id)
-    project.name = name
-    project.priority = priority
-    project.description = description
-    await project.save()
-    res.status(200).json(project)
+    Project.findByPk(id).then(json => {
+        json.name = name
+        json.priority = priority
+        json.description = description
+        json.save().then(json => {
+            res.status(200).json(json)
+        })
+    })
 }
 
-export const deleteProject = async (req, res) => {
+export const deleteProject =  (req, res) => {
     const { id } = req.params
-    const projectUpdate = await Project.destroy({
+    Project.destroy({
         where: {
             id: id
         }
-    });
-    console.log(projectUpdate);
-    res.json(projectUpdate);
+    }).then(json => {
+        console.log(json);
+        res.json(json);
+    })
 }
 
-export const getProjectId = async (req, res) => {
+export const getProjectId =  (req, res) => {
     const { id } = req.params
     try {
 
-        const project = await Project.findByPk(id);
-        if (project === null) {
-            res.status(404).send('Not found!');
-        } else {
-            const project = await Project.findOne({
-                where: {
-                    id: id
-                }
-            });
-            res.status(200).json(project)
-        }
+         Project.findByPk(id).then(response => {
+             if (response === null) {
+                 res.status(404).send('Not found!');
+             } else {
+                Project.findOne({
+                     where: {
+                         id: id
+                     }
+                 }).then(json => {
+                     res.status(200).json(json)
+                 })
+             }
+         })
 
     } catch (error) {
         res.status(404).json({ message: error.message })
     }
 }
 
-export const getProjectTasks = async (req, res) => {
+export const getProjectTasks =  (req, res) => {
     const { id } = req.params
 
     try {
-        const project = await Project.findByPk(id);
-        if (project === null) {
-            res.status(404).send('Not found!');
-        } else {
-            // si existe
-            // buscar las tasks que le corresponden
-            const projectTasks = await Task.findAll({
-                where: {
-                    projectId: id
-                }
-            })
-            res.json(projectTasks)
-        }
+        Project.findByPk(id).then(response => {
+            if (response === null) {
+                res.status(404).send('Not found!');
+            } else {
+                // si existe
+                // buscar las tasks que le corresponden
+               Task.findAll({
+                    where: {
+                        projectId: id
+                    }
+                }).then(response => {
+                    res.json(response)
+                })
+            }
+        })
     } catch (error) {
         res.status(404).json({ message: error.message })
-
     }
 }
